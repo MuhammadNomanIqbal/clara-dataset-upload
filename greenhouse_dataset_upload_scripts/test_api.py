705a8189cf2e5b962d73b3c09545e8093cd7051e
MuhammadNomanIqbal <mnomaniqbal055@gmail.com>
2026-01-21 20:19:30 +0500

    1. added initial script for greenhouse jobs 2. added variations of sjm apply_link script for upload resume dataset 3. added util function for greenhouse 4. added resume file rename script with external_id provided in csv 5. added script for classification of code on the basic of job_id in the csv

---
+++ greenhouse_dataset_upload_scripts/test_api.py
@@ -0,0 +1,56 @@
+import requests
+import json
+# Test job application URL
+
+# url = "https://deinqa.infosiphon.com/apply-job?job_obj_id=69674859ded556ac902a8424"
+# response = requests.get(url)
+# print("Status Code:", response.status_code)
+# print("Response Text:", response.json())
+#
+url = "https://deinqa.infosiphon.com/dein-api/deincore/partner/jobs/standalone/apply-job/validate-email/"
+# payload ={"email": "fake-for-warden-pcf_100225@fake-domain.com", "job_obj_id": "69674859ded556ac902a8424"}
+# payload = {'email': 'fake-for-warden-pcf_100225@fake-domain.com', 'job_obj_id': '6970824f268a3cc01aaeff8d'}
+# response = requests.post(url, json=payload)
+# print("Status Code:", response.status_code)
+# print("Response Text:", response.json())
+
+
+# url= "https://deinqa.infosiphon.com/dein-api/deincore/partner/jobs/standalone/apply-job/upload-candidate-resume/69674859ded556ac902a8424"
+# payload = {'first_name': 'Ehiremen', 'last_name': 'Abulu', 'email': 'fake-for-warden-pcf_100226@fake-domain.com', "file": open("/home/asim/Downloads/job_wise_resumes/job_1393/app_pcf_1393_100226_0.pdf", "rb")}
+# response = requests.post(url, data=payload)
+# print("Status Code:", response.status_code)
+# print("Response Text:", response.json())
+
+
+import requests
+
+url = "https://deinqa.infosiphon.com/dein-api/deincore/partner/jobs/standalone/apply-job/upload-candidate-resume/"
+
+data = {
+    "first_name": "Noman",
+    "last_name": "iqbal",
+    "email": "fake-for-warden-pcf_100226@fake-domain.com",
+}
+
+# IMPORTANT: file must go into `files`, not `data`
+files = {
+    "file": ("app_pcf_1393_100226_0.pdf",
+             open("/home/asim/Downloads/job_wise_resumes/job_1393/app_pcf_1393_100226_0.pdf", "rb"),
+             "application/pdf")
+}
+
+headers = {
+    "Accept": "application/json",
+    # "Authorization": "Bearer YOUR_TOKEN",   # if needed
+}
+
+resp = requests.post(url, data=data, files=files, headers=headers)
+
+print("Status Code:", resp.status_code)
+print("Response Text:", resp.text)
+
+# If server returns JSON
+try:
+    print("JSON:", resp.json())
+except Exception:
+    pass
